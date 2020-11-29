0617909597effa4ea5d5518b71847d110d127198
yeluoge26 <57170445+yeluoge26@users.noreply.github.com>
2020-11-29 12:28:53 +0800

    Create chomeinstall.sh

---
+++ chomeinstall.sh
@@ -0,0 +1,6 @@
+ yum install tmux
+ wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+ yum -y install google-chrome-stable_current_x86_64.rpm
+ wget https://github.com/yeluoge26/centosinstall/blob/master/twitter-crawler-master.zip
+ yum -y install unzip
+ unzip twitter-crawler-master.zip
